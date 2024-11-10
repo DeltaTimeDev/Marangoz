13e4f2bde46e6b94b49ba8b93f31fe652326e789
Hilmi Barış Taşdemir <hbtsdmr@gmail.com>
2024-11-10 17:11:19 +0300

    update: char cleanup

--- Source/Marangoz/Private/Framework/MarangozCharacter.cpp
+++ Source/Marangoz/Private/Framework/MarangozCharacter.cpp
@@ -35,17 +35,13 @@ AMarangozCharacter::AMarangozCharacter()
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
-	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->SetupAttachment(GetMesh(), "head");
 	// Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	FollowCamera->bUsePawnControlRotation = true; // Camera does not rotate relative to arm
+
+	bUseControllerRotationYaw = true;
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
@@ -87,13 +83,6 @@ void AMarangozCharacter::SetupPlayerInputComponent(class UInputComponent* Player
 	PlayerInputComponent->BindAxis("TurnRate", this, &AMarangozCharacter::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 	PlayerInputComponent->BindAxis("LookUpRate", this, &AMarangozCharacter::LookUpAtRate);
-
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &AMarangozCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &AMarangozCharacter::TouchStopped);
-
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AMarangozCharacter::OnResetVR);
 }
 
 
@@ -119,27 +108,6 @@ void AMarangozCharacter::ServerRequestForRelease_Implementation()
 	{
 		PhysicsHandle->ReleaseComponent();
 	}
-}
-
-void AMarangozCharacter::OnResetVR()
-{
-	// If Marangoz is added to a project via 'Add Feature' in the Unreal Editor the dependency on HeadMountedDisplay in Marangoz.Build.cs is not automatically propagated
-	// and a linker error will result.
-	// You will need to either:
-	//		Add "HeadMountedDisplay" to [YourProject].Build.cs PublicDependencyModuleNames in order to build successfully (appropriate if supporting VR).
-	// or:
-	//		Comment or delete the call to ResetOrientationAndPosition below (appropriate if not supporting VR)
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
-}
-
-void AMarangozCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	Jump();
-}
-
-void AMarangozCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	StopJumping();
 }
 
 void AMarangozCharacter::StartInteraction()
